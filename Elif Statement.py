bfa7568da180381261fcb6a6216d92363fe08b36
ryevans5 <69587280+ryevans5@users.noreply.github.com>
2021-01-28 11:48:26 +0000

    Add files via upload

---
+++ Elif Statement.py
@@ -0,0 +1,10 @@
+is_male = True
+is_tall = True
+if is_male and is_tall:
+    print ("You are a tall male")
+elif is_male and not(is_tall):
+    print ("You are a short male") 
+elif not (is_male) and is_tall:
+    print ("You are not a male but you are tall")
+else:
+    print("You are not male or not tall")
